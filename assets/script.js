55892e2e4ccfdc478f6c926b64999457d6d05e64
Zachariah Schneider <zoocko@gmail.com>
2022-10-23 16:57:52 -0400

    This update adds the timeblocks with their basic stylings.

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,3 @@
+// This code adds the current date using moment.js to the #currentDay id in the format "Day, Month, Date of Month".
 var headerDate = moment().format("dddd, MMMM Do");
 $("#currentDay").text(headerDate);
