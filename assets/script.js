151a28ab8954c1ea01f189c03b020ecb09a396d5
Zachariah Schneider <zoocko@gmail.com>
2022-10-24 00:04:49 -0400

    Made some changes to the classes

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,14 @@
+var timeblocks = document.getElementById("timeblocks");
+
 // This code adds the current date using moment.js to the #currentDay id in the format "Day, Month, Date of Month".
 var headerDate = moment().format("dddd, MMMM Do");
 $("#currentDay").text(headerDate);
+
+var currentHour = moment().format("HH")
+console.log(currentHour);
+
+identifyTime = (timeblock) => {
+    array.forEach(element => {
+        
+    });
+}
