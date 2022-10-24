1c540504463ba1a46d1ceb79b8b60be6c39bc4c0
Zachariah Schneider <zoocko@gmail.com>
2022-10-24 00:30:01 -0400

    This update adds a row property to help standardize each row.

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,10 @@
 var timeblocks = document.getElementById("timeblocks");
+var tableHours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
 
 // This code adds the current date using moment.js to the #currentDay id in the format "Day, Month, Date of Month".
 var headerDate = moment().format("dddd, MMMM Do");
 $("#currentDay").text(headerDate);
 
-var currentHour = moment().format("HH")
+var currentHour = moment().format("hA")
 console.log(currentHour);
 
-identifyTime = (timeblock) => {
-    array.forEach(element => {
-        
-    });
-}
