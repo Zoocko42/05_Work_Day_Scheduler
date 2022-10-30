907856bbbb810dff5ae3d73a18742cf1a1248d87
Zachariah Schneider <zoocko@gmail.com>
2022-10-30 15:17:12 -0400

    This update saves entered values to local storage on button-press, and allows saved  values to persist on refresh.

--- assets/script.js
+++ assets/script.js
@@ -15,19 +15,35 @@ var timeblockArray = [nineAM, tenAM, elevenAM, twelveAM, onePM, twoPM, threePM, 
 var headerDate = moment().format("dddd, MMMM Do");
 $("#currentDay").text(headerDate);
 
+//This code adds the current hour in a 24-hour timeframe.
 var currentHour = moment().format("H")
-console.log(currentHour);
-console.log(nineAM.id);
 
+//This code iterates through each timeblock and changes the background color in relation to the current hour.
 for (var i = 0; i < (timeblockArray.length); i++) {
-    if (timeblockArray[i].id == currentHour) {
+    if (parseInt(timeblockArray[i].id) == parseInt(currentHour)) {
         timeblockArray[i].setAttribute("style", "background-color: #ff6961; color: white")
-    } else if (timeblockArray[i].id < currentHour) {
+    } else if (parseInt(timeblockArray[i].id) < parseInt(currentHour)) {
         timeblockArray[i].setAttribute("style", "background-color: #d3d3d3; color: black")
     } else {
         timeblockArray[i].setAttribute("style", "background-color: #77dd77; color: white")
     }
 };
 
-function saveFieldValue () {};
+// This code iterates through each of the timeblocks, checks local storage, and sets the appropriate value.
+timeblockArray.forEach(element => {
+    element.innerHTML = localStorage.getItem(element.id)
+})
+
+// This function is called by a button press in index.html; when the save buttons are pressed, each field is
+// iterated through and its value saved to local storage. The value is not saved if the button is not pressed.
+// The foreach statement above will ensure that any saved value is visible again even on page refresh.
+function saveFieldValue() {
+    timeblockArray.forEach(element => {
+        localStorage.setItem(element.id, element.value)
+    });
+}
+
+
+
+
 
